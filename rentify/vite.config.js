849379133784bc63feb0d7ef4312eea8ac70589e
Asmirkhan007 <asmirkhan007@gmail.com>
2024-05-27 19:35:40 +0530

    hosting env change

--- rentify/vite.config.js
+++ rentify/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // Your backend server
+        target: "https://rentify-backend-production-cac5.up.railway.app", // Your backend server
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
